1ca5b719af6550d73b1da0531a04b04b750ca57f
Harshavardhana <vardhanun333@gmail.com>
2024-07-14 16:59:50 +0530

    code updated

--- src/components/homepage/utils/Content.jsx
+++ src/components/homepage/utils/Content.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { fetchMovies } from './ApiService';
 
 const Content = ({ tabId, searchState, updateSearchState, addRecentSearch }) => {
@@ -11,6 +11,8 @@ const Content = ({ tabId, searchState, updateSearchState, addRecentSearch }) => 
   const [newSearch, setNewSearch] = useState(initialSearchState.query);
   const [searchResults, setSearchResults] = useState(initialSearchState.results);
   const [selectedMovie, setSelectedMovie] = useState(initialSearchState.selectedMovie);
+
+  const inputRef = useRef(null);
 
   useEffect(() => {
     const savedSearchState = {
@@ -33,6 +35,10 @@ const Content = ({ tabId, searchState, updateSearchState, addRecentSearch }) => 
     return () => clearTimeout(timeoutId);
   }, [newSearch]);
 
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
   const handleSearchInput = (e) => {
     setNewSearch(e.target.value);
   };
@@ -47,41 +53,42 @@ const Content = ({ tabId, searchState, updateSearchState, addRecentSearch }) => 
 
   return (
     <div className='w-full max-sm:w-auto h-auto m-[15px] bg-[white] rounded-lg p-4'>
-        <div className='flex justify-center'>
-          <input
-            type='text'
-            value={newSearch}
-            onChange={handleSearchInput}
-            placeholder='Search for a movie...'
-            className='p-[10px] w-[600px] flex border rounded-3xl '
-          />
-        </div>
-        <div className='flex justify-center'>
-          {searchResults.length > 0 && (
-            <div className='border w-[500px] rounded mt-2 bg-white shadow-lg'>
-              {searchResults.map((movie) => (
-                <div
-                  key={movie.imdbID}
-                  onClick={() => handleMovieSelection(movie)}
-                  className='cursor-pointer p-2 hover:bg-gray-200'
-                >
-                  {movie.Title} ({movie.Year})
-                </div>
-              ))}
-            </div>
-          )}
-        </div>
-        <div className='flex justify-center'>
-          {selectedMovie && (
-            <div className='mt-[50px]'>
-              <h2 className='text-2xl font-bold'>{selectedMovie.Title}</h2>
-              <p>Release Year: {selectedMovie.Year}</p>
-              <p>Type: {selectedMovie.Type}</p>
-              <img src={selectedMovie.Poster} alt={selectedMovie.Title} className='mt-4 w-[200px]' />
-            </div>
-          )}
-        </div>
+      <div className='flex justify-center'>
+        <input
+          ref={inputRef}
+          type='text'
+          value={newSearch}
+          onChange={handleSearchInput}
+          placeholder='Search for a movie...'
+          className='p-[10px] w-[600px] flex border rounded-3xl focus:border-orange-400 focus:outline-none focus:shadow-orange-300 focus:shadow-md '
+        />
       </div>
+      <div className='flex justify-center'>
+        {searchResults.length > 0 && (
+          <div className='border w-[500px] rounded mt-2 bg-white shadow-lg'>
+            {searchResults.map((movie) => (
+              <div
+                key={movie.imdbID}
+                onClick={() => handleMovieSelection(movie)}
+                className='cursor-pointer p-2 hover:bg-gray-200'
+              >
+                {movie.Title} ({movie.Year})
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+      <div className='flex justify-center'>
+        {selectedMovie && (
+          <div className='mt-[50px]'>
+            <h2 className='text-2xl font-bold'>{selectedMovie.Title}</h2>
+            <p>Release Year: {selectedMovie.Year}</p>
+            <p>Type: {selectedMovie.Type}</p>
+            <img src={selectedMovie.Poster} alt={selectedMovie.Title} className='mt-4 w-[200px]' />
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 
